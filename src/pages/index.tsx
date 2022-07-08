22530d032d6bf8fcddeb5cd961e9c276a0dfb9c5
TheDunco <duncvan@gmail.com>
2022-07-08 17:21:29 -0400

    No default import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Meta } from "@/layouts/Meta";
 import { Main } from "@/templates/Main";
 import { AppConfig } from "@/utils/AppConfig";
-import ThemeLayout from "@/layouts/ThemeLayout";
+import { ThemeLayout } from "@/layouts/ThemeLayout";
 
 export const Index = () => {
     return (
